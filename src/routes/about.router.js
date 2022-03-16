60dd29d006de15d0b5d2fbd1d4ef2a13de731f89
Christopher Allis <chrisallis109@gmail.com>
2022-03-16 00:07:37 -0400

    updated contact and added services

--- src/routes/about.router.js
+++ src/routes/about.router.js
@@ -7,6 +7,10 @@ router.get('/', (req, res, next) => {
     res.render('partials/about');
 });
 
+router.get('/services', (req, res, next) => {
+    res.render('partials/services');
+});
+
 router.get('/privacy-policy', (req, res, next) => {
     res.render('partials/privacy');
 });
